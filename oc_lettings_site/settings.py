92e4f1bdbec5ae2ccb7f5d20bd7b5778b9398484
Sally PJ <polonio.sally@gmail.com>
2025-03-18 13:19:18 +0100

    Refactor: Modularize project and migrate data
    - Separated `letting` and `profiles` into independent Django apps
    - Updated `INSTALLED_APPS` to include `letting` and `profiels`
    - Moved models (`Letting`, `Address`, `Profile`) to respective apps
    - Created and applied data migration to transfer records from `oc_lettings_site` to new apps
    - Updated imports in views, admin, and URLs to reflect new structure
    - Configured `letting/urls.py` and `profiles/urls.py`, added `include()` in `oc_lettings_site/urls.py`
    - Cleaned up old references and ensured smooth database migration

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -22,6 +22,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'oc_lettings_site.apps.OCLettingsSiteConfig',
+    'letting',
+    'profiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
