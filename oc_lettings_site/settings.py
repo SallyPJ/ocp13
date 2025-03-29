97fdb4d891728fb26c199c2905e0a2415a2068a0
Sally PJ <polonio.sally@gmail.com>
2025-03-30 00:17:21 +0100

    fix: gitlab-ci : add collectstatic on test script setting :  add debug condition to staticfiles_storage dockerfile: delete collectstatics

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -126,7 +126,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static",]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+if not DEBUG:
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 LOGGING = {
